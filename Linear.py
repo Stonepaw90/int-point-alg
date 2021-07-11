cd642f516a2b776b745dc4fdc289b56ebd189c84
AbrahamH <75340597+Stonepaw90@users.noreply.github.com>
2021-07-11 15:05:19 -0400

    Update Linear.py

--- Linear.py
+++ Linear.py
@@ -73,7 +73,7 @@ st.write(
 col = st.beta_columns(2)
 with col[0]:
     gb = GridOptionsBuilder.from_dataframe(input_dataframe)
-    gb.configure_default_column(editable=True)
+    gb.configure_default_column(editable=True, filter = False)
     gb.configure_grid_options(
         editable=True,
         sortable=False,
