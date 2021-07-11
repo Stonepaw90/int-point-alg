e0512823b721b8d63641a3ec9f6803876998cea5
AbrahamH <75340597+Stonepaw90@users.noreply.github.com>
2021-07-11 12:16:19 -0400

    Update Linear.py

--- Linear.py
+++ Linear.py
@@ -169,7 +169,7 @@ if variable_dict["done"]:  #Once solve is pressed
         x_full = x
         c_full = c
         if matrix_full.dot(x_full) != b:
-            st.latex(f"Ax \\neq b, {str(*matrix_full.dot(x_full))} \\neq {str(*b)}")
+            st.latex(f"Ax \\neq b, \hspace{{5px}} {str(*matrix_full.dot(x_full))} \\neq {str(*b)}")
             st.stop()
             # matrix_full = np.concatenate((matrix_small, np.identity(m_s)), axis=1)
             # x_full = np.concatenate((x,s))
