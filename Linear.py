04ff2fe1a6a646564c7d72c51cedd52c2a789a89
AbrahamH <75340597+Stonepaw90@users.noreply.github.com>
2021-07-11 13:19:16 -0400

    Update Linear.py

--- Linear.py
+++ Linear.py
@@ -170,7 +170,7 @@ if variable_dict["done"]:  #Once solve is pressed
         matrix_full = matrix_small
         x_full = x
         c_full = c
-        if any([abs(i) > 0.0001 for i in (matrix_full.dot(x_full) - b)]):
+        if any([abs(i) > 0.001 for i in (matrix_full.dot(x_full) - b)]):
             st.latex(f"Ax \\neq b, \hspace{{8px}} {str(*round_list(matrix_full.dot(x_full)))} \\neq {str(*b)}")
             st.stop()
             # matrix_full = np.concatenate((matrix_small, np.identity(m_s)), axis=1)
@@ -286,7 +286,7 @@ if variable_dict["done"]:  # All branches get here, once data has been verified.
         f = x_full.dot(c_full)
 
         if not variable_dict["standard"]:
-            if any([abs(i) > 0.0001 for i in (matrix_full.dot(x_full) - b)]):
+            if any([abs(i) > 0.001 for i in (matrix_full.dot(x_full) - b)]):
                 st.latex(f"Ax \\neq b, \hspace{{8px}} {str(*round_list(matrix_full.dot(x_full)))} \\neq {str(*b)}")
                 df = pd.DataFrame(data, columns=alist)
                 st.markdown("""
