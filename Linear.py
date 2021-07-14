f4cde9a1277e0d5f1054518de07589780971d795
AbrahamH <75340597+Stonepaw90@users.noreply.github.com>
2021-07-14 09:48:16 -0400

    Update Linear.py

--- Linear.py
+++ Linear.py
@@ -684,11 +684,12 @@ if variable_dict["done"]:
                 fig = plt.figure(figsize=(7,3), dpi = 80)
                 ax = plt.axes()
                 plot_inequalities(matrix_small, b, bbox, ax=ax)
-                go = ax.plot(*df['x'][0], 'go')
+                go = ax.plot(*df['x'][0], 'go', label = "Initial point")
                 for i in range(len(df['x'])-1):
-                    bo = ax.plot(*df['x'][i+1], 'bo')
+                    bo = ax.plot(*df['x'][i+1], 'bo', label = "Improving Point")
                     ax.plot([df['x'][i][0],df['x'][i+1][0]],[df['x'][i][1],df['x'][i+1][1]], 'k-')
-                fig.legend([go, bo], ["Initial", "Improving"])
+                ro = ax.plot(*df['x'][i+1], 'ro', label = "Epsilon-optimal Point")
+                ax.legend(handles = [go, bo, ro])
                
                 plot_space.pyplot(fig)
             except:
